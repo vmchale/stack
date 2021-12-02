c2c7ec3b90e6470d24896d9bd7a17996909b62ce
Vanessa McHale <vamchale@gmail.com>
2021-12-02 13:36:20 -0600

    nothing works LMAO

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 #include <atomic_ops_stack.h>
 
-int main(int argc,  char** argv) {
-    AO_stack_t st;
-    AO_stack_init(&st);
+int main(int argc, char** argv) {
+    AO_stack_t st = AO_STACK_INITIALIZER;
+    /* AO_stack_init(&st); */
     char* str = "yeehaw";
-    AO_stack_push(&st, &str);
+    AO_stack_push(&st, (AO_t *) &str);
     void* ret;
     ret = AO_stack_pop(&st);
     printf("%s\n", (char*) ret);
